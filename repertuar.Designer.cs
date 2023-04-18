b04e270d018ccc2715af4d6e01e9b2e896ce170f
mateusz-szc <mateusz.szc@gmail.com>
2023-04-18 19:27:52 +0200

    Dodawanie pojedynczych filmów do okna repertuar

--- repertuar.Designer.cs
+++ repertuar.Designer.cs
@@ -37,6 +37,8 @@
             button_repertuar_tommorow = new Button();
             button_repertuar_today = new Button();
             button1 = new Button();
+            flowLayoutPanel_repertuar = new FlowLayoutPanel();
+            textBox_rep = new TextBox();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
@@ -70,10 +72,10 @@
             panel1.Controls.Add(button_repertuar_today);
             panel1.Controls.Add(button1);
             panel1.ForeColor = SystemColors.ControlText;
-            panel1.Location = new Point(2, 0);
+            panel1.Location = new Point(12, 0);
             panel1.MinimumSize = new Size(920, 80);
             panel1.Name = "panel1";
-            panel1.Size = new Size(920, 80);
+            panel1.Size = new Size(1410, 80);
             panel1.TabIndex = 0;
             // 
             // button_repertuar_4_days_later
@@ -82,10 +84,11 @@
             button_repertuar_4_days_later.AutoSizeMode = AutoSizeMode.GrowAndShrink;
             button_repertuar_4_days_later.BackColor = Color.IndianRed;
             button_repertuar_4_days_later.Dock = DockStyle.Left;
-            button_repertuar_4_days_later.Location = new Point(720, 0);
-            button_repertuar_4_days_later.MinimumSize = new Size(180, 80);
+            button_repertuar_4_days_later.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button_repertuar_4_days_later.Location = new Point(1128, 0);
+            button_repertuar_4_days_later.MinimumSize = new Size(282, 80);
             button_repertuar_4_days_later.Name = "button_repertuar_4_days_later";
-            button_repertuar_4_days_later.Size = new Size(180, 80);
+            button_repertuar_4_days_later.Size = new Size(282, 80);
             button_repertuar_4_days_later.TabIndex = 6;
             button_repertuar_4_days_later.Text = "button8";
             button_repertuar_4_days_later.UseVisualStyleBackColor = false;
@@ -97,10 +100,11 @@
             button_repertuar_3_days_later.AutoSizeMode = AutoSizeMode.GrowAndShrink;
             button_repertuar_3_days_later.BackColor = Color.IndianRed;
             button_repertuar_3_days_later.Dock = DockStyle.Left;
-            button_repertuar_3_days_later.Location = new Point(540, 0);
-            button_repertuar_3_days_later.MinimumSize = new Size(180, 80);
+            button_repertuar_3_days_later.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button_repertuar_3_days_later.Location = new Point(846, 0);
+            button_repertuar_3_days_later.MinimumSize = new Size(282, 80);
             button_repertuar_3_days_later.Name = "button_repertuar_3_days_later";
-            button_repertuar_3_days_later.Size = new Size(180, 80);
+            button_repertuar_3_days_later.Size = new Size(282, 80);
             button_repertuar_3_days_later.TabIndex = 4;
             button_repertuar_3_days_later.Text = "button7";
             button_repertuar_3_days_later.UseVisualStyleBackColor = false;
@@ -112,10 +116,11 @@
             button_repertuar_2_days_later.AutoSizeMode = AutoSizeMode.GrowAndShrink;
             button_repertuar_2_days_later.BackColor = Color.IndianRed;
             button_repertuar_2_days_later.Dock = DockStyle.Left;
-            button_repertuar_2_days_later.Location = new Point(360, 0);
-            button_repertuar_2_days_later.MinimumSize = new Size(180, 80);
+            button_repertuar_2_days_later.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button_repertuar_2_days_later.Location = new Point(564, 0);
+            button_repertuar_2_days_later.MinimumSize = new Size(282, 80);
             button_repertuar_2_days_later.Name = "button_repertuar_2_days_later";
-            button_repertuar_2_days_later.Size = new Size(180, 80);
+            button_repertuar_2_days_later.Size = new Size(282, 80);
             button_repertuar_2_days_later.TabIndex = 3;
             button_repertuar_2_days_later.Text = "button4";
             button_repertuar_2_days_later.UseVisualStyleBackColor = false;
@@ -127,10 +132,11 @@
             button_repertuar_tommorow.AutoSizeMode = AutoSizeMode.GrowAndShrink;
             button_repertuar_tommorow.BackColor = Color.IndianRed;
             button_repertuar_tommorow.Dock = DockStyle.Left;
-            button_repertuar_tommorow.Location = new Point(180, 0);
-            button_repertuar_tommorow.MinimumSize = new Size(180, 80);
+            button_repertuar_tommorow.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button_repertuar_tommorow.Location = new Point(282, 0);
+            button_repertuar_tommorow.MinimumSize = new Size(282, 80);
             button_repertuar_tommorow.Name = "button_repertuar_tommorow";
-            button_repertuar_tommorow.Size = new Size(180, 80);
+            button_repertuar_tommorow.Size = new Size(282, 80);
             button_repertuar_tommorow.TabIndex = 2;
             button_repertuar_tommorow.Text = "button3";
             button_repertuar_tommorow.UseVisualStyleBackColor = false;
@@ -142,10 +148,11 @@
             button_repertuar_today.AutoSizeMode = AutoSizeMode.GrowAndShrink;
             button_repertuar_today.BackColor = Color.LightSalmon;
             button_repertuar_today.Dock = DockStyle.Left;
+            button_repertuar_today.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
             button_repertuar_today.Location = new Point(0, 0);
-            button_repertuar_today.MinimumSize = new Size(180, 80);
+            button_repertuar_today.MinimumSize = new Size(282, 80);
             button_repertuar_today.Name = "button_repertuar_today";
-            button_repertuar_today.Size = new Size(180, 80);
+            button_repertuar_today.Size = new Size(282, 80);
             button_repertuar_today.TabIndex = 1;
             button_repertuar_today.Text = "button2";
             button_repertuar_today.UseVisualStyleBackColor = false;
@@ -158,19 +165,44 @@
             button1.Size = new Size(75, 23);
             button1.TabIndex = 5;
             // 
+            // flowLayoutPanel_repertuar
+            // 
+            flowLayoutPanel_repertuar.Anchor = AnchorStyles.Top;
+            flowLayoutPanel_repertuar.AutoScroll = true;
+            flowLayoutPanel_repertuar.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            flowLayoutPanel_repertuar.BackColor = SystemColors.ButtonShadow;
+            flowLayoutPanel_repertuar.Location = new Point(38, 116);
+            flowLayoutPanel_repertuar.Margin = new Padding(0);
+            flowLayoutPanel_repertuar.MaximumSize = new Size(1920, 1080);
+            flowLayoutPanel_repertuar.MinimumSize = new Size(800, 580);
+            flowLayoutPanel_repertuar.Name = "flowLayoutPanel_repertuar";
+            flowLayoutPanel_repertuar.Size = new Size(1361, 852);
+            flowLayoutPanel_repertuar.TabIndex = 1;
+            flowLayoutPanel_repertuar.Paint += flowLayoutPanel_repertuar_Paint;
+            // 
+            // textBox_rep
+            // 
+            textBox_rep.Location = new Point(12, 86);
+            textBox_rep.Name = "textBox_rep";
+            textBox_rep.Size = new Size(878, 27);
+            textBox_rep.TabIndex = 3;
+            textBox_rep.TextChanged += textBox_rep_TextChanged;
+            // 
             // repertuar
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
-            ClientSize = new Size(902, 723);
+            ClientSize = new Size(1435, 971);
+            Controls.Add(textBox_rep);
+            Controls.Add(flowLayoutPanel_repertuar);
             Controls.Add(panel1);
             MaximumSize = new Size(1920, 1080);
-            MinimumSize = new Size(920, 770);
+            MinimumSize = new Size(1400, 1018);
             Name = "repertuar";
             StartPosition = FormStartPosition.CenterParent;
             Text = "repertuar";
-            Load += repertuar_Load;
+            
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ResumeLayout(false);
@@ -187,5 +219,7 @@
         private Button button_repertuar_2_days_later;
         private Button button_repertuar_tommorow;
         private Button button_repertuar_4_days_later;
+        private FlowLayoutPanel flowLayoutPanel_repertuar;
+        private TextBox textBox_rep;
     }
 }
--- repertuar.cs
+++ repertuar.cs
@@ -78,14 +78,97 @@ namespace Projekt_kino
             button_repertuar_3_days_later.BackColor = Color.LightSalmon;
         }
 
+        private void dodanie_filmu_do_repertuaru(object sender, EventArgs e,
+            string Film_Title,
+            //string Film_Content,
+            //DateTime Film_DataDodania,
+            //int Film_CatID,
+            int Film_Duration,
+            //int Film_ID,
+            string Film_Language
+            //string Film_Production,
+            //string Film_Translation
+            //List<string>? Film_Cateogry = null,
+            
+            
+            
+            )
+        {
+            Panel panel = new Panel();
+
+            panel.BackColor = Color.AliceBlue;
+            panel.Size = new Size(flowLayoutPanel_repertuar.ClientSize.Width - 7, 300);
+
+            //TYTUL string Film_Title
+            panel.Controls.Add(new LinkLabel
+            {
+                Text = Film_Title,
+                LinkColor = Color.Black,
+                Font = new Font("Arial", 16, FontStyle.Bold),
+                AutoSize = true,
+                
+            });
+
+            // JEZYK string Film_Language
+            panel.Controls.Add(new Label
+            {
+                Text = Film_Language,
+                ForeColor = Color.Black,
+                Font = new Font("Arial", 16, FontStyle.Bold),
+                AutoSize = true,
+                Left = 1000,
+
+            });
+
+            // CZAS TRWANIA int Film_Duration,
+            panel.Controls.Add(new Label
+            {
+                Text = Film_Duration.ToString() + " " + " MIN",
+                ForeColor = Color.Black,
+                Font = new Font("Arial", 16, FontStyle.Bold),
+                AutoSize = true,
+                Left = 1200,
+            });
+
+
+
+
+            flowLayoutPanel_repertuar.Controls.Add(panel);
+
+        }
+
         private void button_repertuar_4_days_later_Click(object sender, EventArgs e)
         {
             zmiana_kolorow_przyciskow_dni();
             button_repertuar_4_days_later.BackColor = Color.LightSalmon;
-            //button_repertuar_2_days_later.Text = button_repertuar_4_days_later.Text;   
+
+            dodanie_filmu_do_repertuaru(sender, e, "terminator", 126, "DUB PL");
+
         }
 
         private void repertuar_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void flowLayoutPanel_repertuar_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+        private void textBox_rep_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel_repertuar_film_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel_repertuar_film_tytul_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
 
         }
